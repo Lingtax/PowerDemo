5ffeed2ae5deb829c1923794bd2f01488bdd3f04
Lingtax <Lingtax@users.noreply.github.com>
2016-12-07 00:21:36 +1100

    Add summary and critical value functionality
    Added a critical value variable allowing the test of influence of critical values as well as effect size and sample on power. 
    Critical value updates on histogram
    
    Added a text summary of simulation outputs.

--- server.R
+++ server.R
@@ -3,30 +3,39 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-
-
-  pval <- NULL
+  out <- data.frame()
   
   output$distPlot <- renderPlot({
+    
     for(i in 1:2500){
       c <- c(rep(1, input$n), rep(2, input$n))
       x <- c(rnorm(input$n, mean=0), rnorm(input$n, mean=input$d))
       t <- t.test(x~c)
-      pval[i] <- t$p.value 
+      
+      out[i,1] <<- t$statistic
+      out[i,2] <<- t$parameter
+      out[i,3] <<- t$p.value 
+      out[i,4] <<- t$estimate[2] - t$estimate[1] # How much higher is group 2 than group 1
     }
     
-    
-    
+    names(out) <<- c("t", "df", "pval", "x2-x1")
+
     # draw the histogram with the specified number of bins
-    hist(pval[pval<=input$plotmax], breaks = 20, xlim=c(0, input$plotmax), col = 'darkgray', border = 'white', 
+    hist(out$pval[out$pval<=input$plotmax], breaks = 20, xlim=c(0, input$plotmax), col = 'darkgray', border = 'white', 
          main = paste("Histogram of p values in 2500 t-tests \nwith a true mean difference of ", input$d, "and ", 
                       input$n, " cases per group."), xlab = "p value", ylab= "Number of trials")
-    abline(v=0.05, col="red")
+    abline(v=input$crit, col="red")
   })
+  
+  output$text1 <- renderText({
+    if (input$d==0){
+      paste("In 2500 simulated trials, sampling two groups of ", input$n, " individuals from a single population of mean = 0, ",
+            sum(out$pval<input$crit), " statistically significant mean differences (", (sum(out$pval<input$crit)/2500)*100, 
+            "% of trials) were observed (alpha = ", input$crit  ,").", sep="")
+    } else {
+          paste("In 2500 simulated trials, sampling two groups of ", input$n, " individuals each from populations of mean = 0 and ", 
+                input$d, ", ",  sum(out$pval<input$crit), " statistically significant mean differences (", 
+                (sum(out$pval<input$crit)/2500)*100, "% of trials) were observed (alpha = ", input$crit  ,").", sep="") 
+    }
+    })
 })
